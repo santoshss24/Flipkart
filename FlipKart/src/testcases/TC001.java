c5ab32d7a389da674aac988ea2a224553f3bd499
Hemanth4sure <66875908+Hemanth4sure@users.noreply.github.com>
2020-06-17 19:27:38 +0530

    Update TC001.java

--- FlipKart/src/testcases/TC001.java
+++ FlipKart/src/testcases/TC001.java
@@ -11,14 +11,14 @@ public class TC001 extends BaseTest
 	{
 		HomePage home = new HomePage(driver, webActionUtil);
 		home.clickOnDressesLink();
-		home.selectProduct("4");
-		ProductPage productPage = home.clickMoreButton("4");
+		home.selectProduct("5");
+		ProductPage productPage = home.clickMoreButton("5");
 		productPage.clickPlusButton(3);
 		productPage.clickMinusButton(1);
 		productPage.selectSize("L");
-		productPage.selectColor("Pink");
+		productPage.selectColor("Blue");
 		productPage.clickOnAddToCart();
 		OrderPage orderPage = productPage.clickOnProceedToCheckOut();;
-		Assert.assertTrue(orderPage.verifyProductIsDiplayed("4"));
+		Assert.assertTrue(orderPage.verifyProductIsDiplayed("5"));
 	}
 }
